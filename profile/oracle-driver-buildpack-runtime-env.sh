3e2c8f2f89ddcafe46679d81308a82a3e8f0f4db
Abdurrasheed99 <74869505+Abdurrasheed99@users.noreply.github.com>
2024-03-13 14:03:54 +0530

    Update oracle-driver-buildpack-runtime-env.sh

--- profile/oracle-driver-buildpack-runtime-env.sh
+++ profile/oracle-driver-buildpack-runtime-env.sh
@@ -1,4 +1,4 @@
-export ORACLE_HOME="$HOME/.oracle-build/"
+export ORACLE_HOME="$HOME/.oracle-build"
 export LD_LIBRARY_PATH=$ORACLE_HOME/instantclient:$ORACLE_HOME/libaio:/lib/x86_64-linux-gnu
 export OCI_LIB_DIR=$ORACLE_HOME/instantclient
 export OCI_INC_DIR=$ORACLE_HOME/instantclient/sdk/include
